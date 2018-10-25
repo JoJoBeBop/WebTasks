65f12ad2a0577ac11e123d003852d848f160db5c
Jori Blom <jorib@ety-a2123b-23.edu.wks.metropolia.fi>
2018-10-25 12:13:01 +0300

    toimi

---
+++ untitled1/esimJavaS.js
@@ -0,0 +1,42 @@
+// 1.
+//alert('Tämä on JS-ohjelma');
+
+//2.
+/*
+let nimi;
+nimi = prompt('Anna nimesi');
+alert('Moro moro, ' + nimi);*/
+
+// 3.
+/*let sade;
+sade = prompt ("Anna ympyrän säde");
+let pAla = Math.PI * Math.pow(sade, 2);
+alert(pAla);*/
+
+//4.
+/*let kanta, korkeus;
+kanta = prompt("Anna kanta: ");
+korkeus = prompt("Anna korkeus: ");
+let piiri;
+piiri = 2 * kanta + 2 * korkeus;
+let pAla;
+pAla = kanta * korkeus;
+alert("Pinta-ala on: " + pAla + ". " + "Piiri on: " + piiri);*/
+
+// 5.
+/*let num1, num2, num3;
+num1 = prompt("Anna ensimmäinen luku: ");
+num2 = prompt("Anna toinen luku: ");
+num3 = prompt("Anna kolmas luku: ");
+let summa, tulo, keskiarvo;
+summa = +num1 + +num2 + +num3;
+tulo = num1 * num2 * num3;
+keskiarvo = (num1 + num2 + num3) / 3;
+alert("Lukujen summa: " + summa + ". Lukujen tulo: " + tulo + ". Lukujen keskiarvo: " + keskiarvo);*/
+
+
+
+
+
+
+
