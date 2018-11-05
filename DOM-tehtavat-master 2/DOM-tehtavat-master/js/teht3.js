7001809a3be5fa751785ebc02c9563681d62f39f
Jori Blom <jorib@ety-a2123b-23.edu.wks.metropolia.fi>
2018-11-05 14:07:41 +0200

    man

--- DOM-tehtavat-master 2/DOM-tehtavat-master/js/teht3.js
+++ DOM-tehtavat-master 2/DOM-tehtavat-master/js/teht3.js
@@ -1,98 +1,17 @@
 let kuvat = [
-    'http://placekitten.com/321/240',
-    'http://placekitten.com/320/241',
-    'http://placekitten.com/322/242',
-    'http://placekitten.com/321/240',
-    'http://placekitten.com/331/240',
+  'http://placekitten.com/321/240',
+  'http://placekitten.com/320/241',
+  'http://placekitten.com/322/242',
+  'http://placekitten.com/321/240',
+  'http://placekitten.com/331/240',
 ];
 
 let i;
-let photos = "";
-/*
-
-const ul = document.querySelector("ul");
-
-const html =
-    `<li>
-            const[i]
-    </li> `;
-ul.innerHTML = html;
-*/
-/*
-
-for (i = 0; i <= 5; i++) {
-    photos = "<li>" + kuvat[I] + "</li>";
-    document.getElementById("name").innerHTML += photos;
-}
-
-
-for (i = 0; i <= 5; i++) {
-
-
-
-    const ul = document.querySelector("#jotain");
-
-    const html =
-           `<li>
-                  <img src="kuvat[i]">
-             </li> `;
-    ul.innerHTML = html;
-
-
-
-kyrpa = kuvat;
-
-for (i = 1; i <= 5; i++) {
-    const div = document.querySelector('#jotain');          // haetaan elementti, jonka id on esimerkki
-    const html =                                               // tehdään monirivinen merkkijono, huomaa backtick merkkijonon ympärillä (gravis)
-        `<p>
-            Tässä on kappale tekstiä, jossa on kuva.
-
-         </p>`;
-
-}
-div.innerHTML = html;
-
-
-}
-
-*/
-
-/*
-
-for (i = 1; i <= 5; i++) {
-
-    let kuvat = [
-        'http://placekitten.com/321/240',
-        'http://placekitten.com/320/241',
-        'http://placekitten.com/322/242',
-        'http://placekitten.com/321/240',
-        'http://placekitten.com/331/240',
-    ];
-
-
-    const ul = document.querySelector("#jotain");
-
-    const html = `<li><img src="http://placekitten.com/321/240"></li> `;
-    ul.innerHTML = html;
-
-
-
-let list = document.getElementsByTagName("ul")[0];
-list.getElementsByTagName("li")[0].innerHTML = "Milk";
-
-const pol = document.getElementsByTagName('UL');
-const html = `<p> Tässä on kappale tekstiä, jossa on kuva."></p>`;
-pol.innerHTML = html;
-
-}
-*/
-
 
 for (i = 1; i <= kuvat.length; i++) {
-    photo.src = kuvat[i];
 
-    document.getElementById('jotain').innerHTML += "<li>" + "<img src=\"http://placekitten.com/321/240\">" ;
+  document.querySelector('ul').innerHTML += '<li>' + '<img src="' + kuvat[i] + '">';
+
 }
 
 
--- DOM-tehtavat-master 2/DOM-tehtavat-master/js/teht4.js
+++ DOM-tehtavat-master 2/DOM-tehtavat-master/js/teht4.js
@@ -7,29 +7,22 @@ const kuvat = [
 ];
 
 let i;
-
-
+const uli = document.querySelector('ul');
 
 for (i = 0; i < kuvat.length; i++) {
 
+  const photo = document.createElement('img');
+  photo.src = kuvat[i];
 
+  const li = document.createElement('li');
+  li.appendChild(photo);
 
-    const uli = document.querySelector("ul");
-
-    const photo = document.createElement("img");
-    photo.src = kuvat[i];
-
-    const li = document.createElement('li');
-    li.appendChild(photo);
-
-    uli.appendChild(li);
-
-
+  uli.appendChild(li);
 
 }
 
-document.querySelector("ul").style = "list-style-type: none;";
-i = 0;
+document.querySelector('ul').style = 'list-style-type: none;';
+
 /*
 
 for (i = 0; i < kuvat.length; i++) {
@@ -38,5 +31,6 @@ for (i = 0; i < kuvat.length; i++) {
 }
 */
 
-document.querySelector("li:nth-child(2n+1)").style = "opacity: 0.5;";
+//Ei toimi, mutta aiak lähellä tai jotain
+//document.querySelector('li:nth-child(2n+1)').style = 'opacity: 0.5;';
 
