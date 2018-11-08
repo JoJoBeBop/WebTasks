35523a9709f3c6f00b0aad705a94c8db885d8fa9
Jori Blom <jorib@ety-a2123b-23.edu.wks.metropolia.fi>
2018-11-08 14:13:26 +0200

    TO

--- event-tehtavat-master/js/teht4A.js
+++ event-tehtavat-master/js/teht4A.js
@@ -12,3 +12,65 @@ const pics = [
     big: 'https://placeimg.com/640/480/tech',
   },
 ];
+
+
+for (let j = 0; j < pics.length; j++) {
+  const kuva = document.createElement('img');
+  const li = document.createElement('li');
+  const isoKuva = document.querySelector('#kuvaPaikka');
+
+  kuva.src = pics[j].thumb;
+  li.appendChild(kuva);
+  document.querySelector('#lista').appendChild(li);
+
+  li.addEventListener('click', (evt) => {
+    document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[j].big + '">';
+    //CSS
+    document.querySelector('#kuvaPaikka').style = "display: block;";
+    //document.querySelector('#kuvaPaikka').setAttribute('class', 'hidden');
+
+  });
+
+  isoKuva.addEventListener("click", (poista) => {
+    document.querySelector('#kuvaPaikka').style = "display: none;";
+    });
+
+
+}
+
+
+
+
+
+
+
+
+/*
+
+document.querySelector('li:nth-child(1)').addEventListener("click", print1);
+document.querySelector('li:nth-child(2)').addEventListener("click", print2);
+document.querySelector('li:nth-child(3)').addEventListener("click", print3);
+
+function print1() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[0].big + '">';
+}
+
+function print2() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[1].big + '">';
+}
+
+function print3() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[2].big + '">';
+}
+
+document.querySelector('#testi').style = "color: #eee; background-color: #222; padding: 3rem;";
+
+*/
+
+
+
+
+
+
+
+
--- event-tehtavat-master/js/teht4B.js
+++ event-tehtavat-master/js/teht4B.js
@@ -12,3 +12,53 @@ const pics = [
     big: 'https://placeimg.com/640/480/tech',
   },
 ];
+
+//Thumbnailit
+for (let i = 0; i < pics.length; i++){
+  document.querySelector('ul').innerHTML += '<li>' + '<img src="' + pics[i].thumb + '">';
+
+}
+
+document.querySelector('li:nth-child(1)').addEventListener("click", print1);
+document.querySelector('li:nth-child(2)').addEventListener("click", print2);
+document.querySelector('li:nth-child(3)').addEventListener("click", print3);
+
+function print1() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[0].big + '">';
+  document.querySelector('div').setAttribute('class', 'visible');
+
+}
+
+function print2() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[1].big + '">';
+  document.querySelector('div').setAttribute('class', 'visible');
+
+}
+
+function print3() {
+  document.querySelector('#kuvaPaikka').innerHTML = '<img src="' + pics[2].big + '">';
+  document.querySelector('div').setAttribute('class', 'visible');
+
+}
+
+//CSS Piilotus
+document.querySelector('#kuvaPaikka').addEventListener("click", piilota);
+
+function piilota() {
+  document.querySelector('div').setAttribute('class', 'hidden');
+
+}
+
+
+//document.querySelector('#testi').style = "color: #eee; background-color: #222; padding: 3rem;";
+//  document.querySelector('#testi').setAttribute('class', 'vari');
+
+
+
+
+
+
+
+
+
+
