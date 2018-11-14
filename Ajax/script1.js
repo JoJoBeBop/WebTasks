48d2f7c42b047881e7be57497deeaf4f72ed55e3
Jori Blom <jorib@ety-a2123b-23.edu.wks.metropolia.fi>
2018-11-14 15:43:43 +0200

    KE

--- Ajax/script1.js
+++ Ajax/script1.js
@@ -4,10 +4,27 @@ const haku = document.getElementById("hakuteksti");
 function A(evt){
   console.log(haku.value);
   fetch("http://api.tvmaze.com/search/shows?q=" + haku.value)
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+
+      .then(function(response) {
+        return response.text();
+
+
+      })
+      .then(function(html) {
+        let toHTML = new DOMParser();
+        let parse = toHTML.parseFromString(html, "text/html");
+
+
+        console.log(parse);
+      })
+      .catch(function(errorLog) {
+        console.log("ERROR : ", errorLog);
+      });
 }
 nappi.addEventListener('click', A);
+
+
 
 
 
@@ -15,3 +32,22 @@ nappi.addEventListener('click', A);
 /*
 const ul = document.getElementById('demo'); // Get the list where we will place our authors
 const url = "http://api.tvmaze.com/search/shows?q=" + haku.value;*/
+/*
+
+Toimii:
+
+    function A(evt){
+      console.log(haku.value);
+      fetch("http://api.tvmaze.com/search/shows?q=" + haku.value)
+
+
+      .then(function(response) {
+        return response.json();
+
+      })
+      .then(function(data) {
+        console.log(data)
+      })
+    }
+nappi.addEventListener('click', A);
+*/
