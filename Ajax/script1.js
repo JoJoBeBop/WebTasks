c8993a74912f2187004ffe96f8aeb0bbfbd0772a
Jori Blom <jorib@ety-a2123b-23.edu.wks.metropolia.fi>
2018-11-16 11:30:56 +0200

    PE

--- Ajax/script1.js
+++ Ajax/script1.js
@@ -41,12 +41,31 @@ function nayta(tulot) {
     const linkki = tulot[i].show.url;
     const yhteenveto = tulot[i].show.summary;
     const genre = tulot[i].show.genres;
-    const kuva = tulot[i].show.image.medium;
+
+
+
+
+
 
     //Printtaa
-    const div5 = document.querySelector('ul');
-    const html5 = '<img src="' + kuva + '">';
-    div5.innerHTML += html5;
+
+    const divide = document.querySelector('.tulostusDiv');
+    const divi = '<div>';
+    divide.innerHTML += divi;
+
+    //Katsoo jos kuva on Null
+    if (tulot[i].show.image != null ){
+      let kuva = tulot[i].show.image.medium;
+      const div5 = document.querySelector('ul');
+      const html5 = '<img src="' + kuva + '">';
+      div5.innerHTML += html5;
+
+    } else {
+      const kuva = "https://cdn.releases.com/img/image/6571e6ee-a45a-4b44-b736-9cefa0493180.jpg/300"
+      const div5 = document.querySelector('ul');
+      const html5 = '<img src="' + kuva + '">';
+      div5.innerHTML += html5;
+    }
 
     const div = document.querySelector('ul');
     const html = '<li>' + nimi + '</li>';
@@ -64,6 +83,10 @@ function nayta(tulot) {
     const html4 = '<li>' + genre[0] + ', ' + genre[1] + ', ' + genre[2] +
         '</li> -------------------------- <br>';
     div4.innerHTML += html4;
+
+    const divide1 = document.querySelector('.tulostusDiv');
+    const divi1 = '</div>';
+    divide1.innerHTML += divi1;
 
   }
   console.log('fff');
